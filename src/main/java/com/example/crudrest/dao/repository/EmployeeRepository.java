038fd5a72f0c0a4b6a36f8ffb46024dd476ac71d
maksim <maksim@voicecom.ee>
2022-04-28 09:41:00 +0300

    ...

--- src/main/java/com/example/crudrest/dao/repository/EmployeeRepository.java
+++ src/main/java/com/example/crudrest/dao/repository/EmployeeRepository.java
@@ -4,7 +4,8 @@ import com.example.crudrest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-// Now with DATA REST no Controller, DAO and Service Implementation required
+// With DATA REST no Controller, DAO and Service Implementation required
+// However this is applicable ONLY to backend purposes. In web application DATA REST has no meaning
 // By default Spring will create endpoints based on Entity type - first character of Entity in lowercase + "s" at the end
 @RepositoryRestResource(path = "members") // <- Custom endpoint name, /employees -> /members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { // <- Entity type, Primary key type
